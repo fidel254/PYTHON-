9b1af5695a148a56f85654c1b87084519879e927
fidel kiptoo <fidelkiptoo@gmail.com>
2022-03-15 17:47:24 +0300

    First commit

---
+++ madlib.py
@@ -0,0 +1,7 @@
+name = input("Name: ")
+Age = input("Age: ")
+Course = input("Course: ")
+
+madlib = f"Hello {name}, You are {Age} years old.\
+Your Course is {Course}. Welcome to KCA University! The source of knowledge"
+print(madlib)
